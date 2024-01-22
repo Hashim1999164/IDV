2232fffdba8dd4ee7d1d07565e72c10eb479c69e
Hashim Khan <hashim@aiondigital.com>
2024-01-22 14:10:21 +0500

    no message

---
+++ IDV.framework/Headers/IDV.h
@@ -0,0 +1,18 @@
+//
+//  IDV.h
+//  IDV
+//
+//  Created by Bilal Ahmad on 31/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDV.
+FOUNDATION_EXPORT double IDVVersionNumber;
+
+//! Project version string for IDV.
+FOUNDATION_EXPORT const unsigned char IDVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDV/PublicHeader.h>
+
+
